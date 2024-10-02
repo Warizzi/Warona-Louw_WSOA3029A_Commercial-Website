c0fb6751730f16a71f234fd85f45662d368b6844
Warizzi <2559188@students.wits.ac.za>
2024-10-02 16:19:42 +0200

    footer injection tests

---
+++ Scripts/footer.js
@@ -0,0 +1,12 @@
+
+window.onload = function() {
+    const footer = document.createElement("footer"); //Creating footer element
+footer.id = "footer";
+
+//Footer content
+footer.innerHTML = "<p>@copyright Warona Louw | All rights reserved</p>";
+
+//Append footer to body
+document.body.appendChild(footer);
+
+};
